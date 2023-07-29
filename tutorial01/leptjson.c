e6301aa4453b4e4d3822b3e83214b8cffe681fc2
McGrady <McGrady47@163.com>
2023-07-29 11:28:50 +0800

    add my answer 100%pass

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -33,7 +33,7 @@ static int lept_parse_false(lept_context* c, lept_value* v) {
     
 }static int lept_parse_true(lept_context* c, lept_value* v) {
     EXPECT(c, 't');
-    if (c->json[0] != 'u' || c->json[1] != 'r' || c->json[2] != 'e')
+    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
         return LEPT_PARSE_INVALID_VALUE;
     c->json += 3;
     v->type = LEPT_TRUE;
@@ -52,8 +52,8 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
 
 int lept_parse(lept_value* v, const char* json) {
     lept_context c;
-    assert(v != NULL);
     int ret;
+    assert(v != NULL);
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
